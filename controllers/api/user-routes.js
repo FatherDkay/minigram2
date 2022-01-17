97811a9005caa1a63fe7967de6cf8dd889d3a470
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-16 16:45:31 -0800

    logout working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -140,4 +140,16 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+// Logout user
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
